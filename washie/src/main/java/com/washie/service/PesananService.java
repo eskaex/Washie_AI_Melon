ad97e8aa4266179fc7d6ea2cc249d689aa64eb91
gracevln <grace.violin@ti.ukdw.ac.id>
2026-04-16 19:44:01 +0700

    register controller

---
+++ washie/src/main/java/com/washie/service/PesananService.java
@@ -0,0 +1,2 @@
+public class PesananService {
+}
--- washie/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.washie.washie {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jakarta.persistence;
-
-
-    opens com.washie.washie to javafx.fxml;
-    exports com.washie.washie;
-}
